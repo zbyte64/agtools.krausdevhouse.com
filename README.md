da400c8fde466b5904462c14b44a1f063dadbc5b
Jason Kraus <zbyte64@gmail.com>
2024-03-17 18:13:57 -0700

    wire up historical-eto endpoint

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Installing
 Running
 -------
 `docker compose run web python -m agtools.download_cimis_spatial`
+
+
+Usage
+-----
+
+`curl http://localhost:8000/historical-eto?lat=36.7664498&lon=-119.8054374&start_date=2024-03-15&num_of_days=1`
--- agtools/main.py
+++ agtools/main.py
@@ -1,4 +1,17 @@
-from fastapi import FastAPI
+from datetime import date, timedelta
+from fastapi import Depends, FastAPI, HTTPException, status
+
+# from geoalchemy2 import Geometry
+from geoalchemy2.functions import (
+    ST_Distance,
+    ST_GeographyFromText,
+)
+from sqlalchemy import and_, select
+from sqlalchemy.sql.functions import sum
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from agtools.database import get_async_session
+from agtools.models import CIMISDailyPoints
 
 
 app = FastAPI()
@@ -7,3 +20,37 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/historical-eto")
+async def get_historical_eto(
+    lat: float,
+    lon: float,
+    start_date: date,
+    num_of_days: int,
+    db_session: AsyncSession = Depends(get_async_session),
+):
+    if num_of_days < 1:
+        raise HTTPException(
+            status=status.HTTP_400_BAD_REQUEST,
+            detail="num_of_days must be positive",
+        )
+    end_date = start_date + timedelta(days=num_of_days)
+    # assumes the geometry points are stable through time
+    daily_values = (
+        select(CIMISDailyPoints.value_inches)
+        .where(
+            and_(CIMISDailyPoints.date >= start_date, CIMISDailyPoints.date <= end_date)
+        )
+        .order_by(
+            ST_Distance(
+                CIMISDailyPoints.geometry,
+                ST_GeographyFromText("POINT({} {})".format(lon, lat)),
+            )
+        )
+        .limit(num_of_days)
+    ).subquery()
+    total_eto = select(sum(daily_values.c.value_inches))
+
+    result = await db_session.execute(total_eto)
+    return result.scalar_one()
